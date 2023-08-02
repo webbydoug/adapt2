17b23bc30b9a81a941fc55380c4b8c8d0119d265
Doug Ouverson <doug@ouverson.com>
2023-08-02 10:50:00 -0400

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://adapt2.com',
+  integrations: [tailwind(), partytown(), image(), sitemap(), mdx()]
+});
